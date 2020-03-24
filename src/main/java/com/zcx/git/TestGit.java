3b65b215700389263ad031a841a85fe5ae3d7d67
zhuchunxu <2961378664@qq.com>
2020-03-24 20:29:46 +0800

    第二次提交

--- src/main/java/com/zcx/git/TestGit.java
+++ src/main/java/com/zcx/git/TestGit.java
@@ -4,5 +4,7 @@ public class TestGit {
 	
 	public static void main(String[] args) {
 		System.out.println("hellow,git!");
+		System.out.println("hellow,git!");
+		System.out.println("hellow,git!");
 	}
 }
